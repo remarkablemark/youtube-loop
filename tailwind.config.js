1edeb932d320987ec13f734f766b202e7ec146ea
Mark <mark@remarkablemark.org>
2023-09-04 11:34:12 -0400

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
