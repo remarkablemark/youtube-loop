96dd0b76d87bf4134f8e973335ce4142da08f0ca
Mark <mark@remarkablemark.org>
2023-09-04 12:13:28 -0400

    feat: render logo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,36 +1,12 @@
-import './app.css';
-
-import { useState } from 'preact/hooks';
-
-import viteLogo from '/vite.svg';
-
-import preactLogo from '../../assets/preact.svg';
+import YouTube from '../YouTube';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <main class="flex justify-center">
+      <h1 class="flex items-center">
+        <YouTube width={300} />
+        <span class="ml-3 text-7xl tracking-tight">Loop</span>
+      </h1>
+    </main>
   );
 }
