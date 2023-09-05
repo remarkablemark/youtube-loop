5b5282fdb73bf5b1ed90057bbd2414975f8e5391
Mark <mark@remarkablemark.org>
2023-09-04 23:17:51 -0400

    refactor: rename tailwind.config.js to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
-// eslint-disable-next-line tsdoc/syntax
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
