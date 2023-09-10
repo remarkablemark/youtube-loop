2d59d3e1991904faedba8db6c3fa3d39d7eccbae
Mark <mark@remarkablemark.org>
2023-09-10 14:46:59 -0400

    chore(components): remove "Go" button in Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,7 +3,7 @@ export default function Input() {
     <div class="flex justify-center">
       <form class="container mx-4">
         <label
-          for="go"
+          for="input"
           class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Paste YouTube URL
@@ -29,19 +29,12 @@ export default function Input() {
           </div>
 
           <input
-            type="search"
-            id="go"
             class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
+            id="input"
             placeholder="Paste YouTube URL"
             required
+            type="search"
           />
-
-          <button
-            type="submit"
-            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Go
-          </button>
         </div>
       </form>
     </div>
