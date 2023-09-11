c40e80124e609e51b6c483ccf80f0f0fea089dd1
Mark <mark@remarkablemark.org>
2023-09-10 23:09:48 -0400

    build: rename env var BASE to VITE_BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE || '',
+  base: process.env.VITE_BASE_URL || '',
   plugins: [preact()],
 });
