dd15ee862454723629fdbf486d74a689c6691a4c
Mark <mark@remarkablemark.org>
2023-09-17 13:16:50 -0400

    build: set darkMode to 'class' in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,11 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  plugins: [],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
 
 export default config;
